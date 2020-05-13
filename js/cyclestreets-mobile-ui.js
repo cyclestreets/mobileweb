cef01f07006fdbef86c5a303c76536e8404db153
Siequnu <11821606+Siequnu@users.noreply.github.com>
2020-05-13 16:49:00 +0800

    Refactor account creation CSS

--- js/cyclestreets-mobile-ui.js
+++ js/cyclestreets-mobile-ui.js
@@ -400,7 +400,7 @@ var cyclestreetsui = (function ($) {
 			// While developing, shortcut to certain panels on load
 			//$('#route-search-panel').hide();
 			//$('#ride-notification').delay(2000).slideDown('slow');
-			$('#create-account-panel').show();
+			$('#creating-account-panel').show();
 			
 			
 		}

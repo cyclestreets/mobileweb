e374c49bf3eda94fad7d94a234ff1299bec9292d
Siequnu <11821606+Siequnu@users.noreply.github.com>
2020-05-13 17:33:00 +0800

    Refactor settings and map-style panels

--- js/cyclestreets-mobile-ui.js
+++ js/cyclestreets-mobile-ui.js
@@ -400,7 +400,7 @@ var cyclestreetsui = (function ($) {
 			// While developing, shortcut to certain panels on load
 			//$('#route-search-panel').hide();
 			//$('#ride-notification').delay(2000).slideDown('slow');
-			$('#creating-account-panel').show();
+			$('#map-style-panel').show();
 			
 			
 		}

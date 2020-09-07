afcca75163a01e683387e6107934cea4c319f10e
Siequnu <11821606+Siequnu@users.noreply.github.com>
2020-09-07 14:44:04 +0800

    Photomap: share button works with correct data

--- js/cyclestreets.js
+++ js/cyclestreets.js
@@ -304,7 +304,7 @@ var cyclestreetsui = (function ($) {
 				cyclestreetsui.displayPhotomapPopup (renderDetailsHtml, animation);
 			},
 			popupHtml: 
-			      '<div class="data" data-coordinates="{geometry.coordinates}"></div><div class="inner-card flip-card-inner"><div class="flip-card-front popup-card"><a href="#" class="ui-button close-button" title="Close this popup"><img src="/images/icon-cross-red.svg" alt="Close icon" /></a><img class="popup-photo" src="{properties.thumbnailUrl}" alt="Photo" /><a href="#" class="get-directions" title="Get directions to this place"><img class="get-directions" src="/images/btn-get-directions-large.svg" /></a>' 
+			      '<div class="data" data-coordinates="{geometry.coordinates}" data-location-id="{properties.id}" data-caption="{properties.caption}" data-username="{properties.username}"></div><div class="inner-card flip-card-inner"><div class="flip-card-front popup-card"><a href="#" class="ui-button close-button" title="Close this popup"><img src="/images/icon-cross-red.svg" alt="Close icon" /></a><img class="popup-photo" src="{properties.thumbnailUrl}" alt="Photo" /><a href="#" class="get-directions" title="Get directions to this place"><img class="get-directions" src="/images/btn-get-directions-large.svg" /></a>' 
 				+ '<p class="key">{properties.caption}</p><a class="share" href="#" title="Share this location"><img src="/images/icon-share.svg" alt="Share icon" /> Share</a><a class="flip" href="#" title="Show more information"> Photo info</a></div>'
 				+ '<div class="flip-card-back popup-card"><a href="#" class="back" title="Return to the front of this card"><img src="/images/icon-disclosure-red-left.svg" alt="Left chevron" /></a><br>' 
 				+ '<p class="key">Category: </p><p>{properties.categoryName}</p><br>	<p class="key">Type: </p><p>{properties.metacategoryName}</p><hr /><ul><li><img src="/images/icon-user.svg" alt="User icon" /><p>{properties.username}</p></li>'
@@ -1716,19 +1716,20 @@ var cyclestreetsui = (function ($) {
 					});
 				}
 			});
-
 					
 			// Enable the share sheet 
 			$(document).on ('click', '.popup.photomap a.share', function (event) {
-				console.log (event);
-				
-				// #¡# Need to get the photo metadata and populate this shareData
+				// Get location data
+				var data = $(event.target).parents ('.popup').find ('.data').data ();
+				var locationId = data.locationId;
+				var caption = data.caption;
+				var username = data.username;
+
 				const shareData = {
-					title: 'Photo intelligence',
-					text: 'View a photo about cycling!',
-					url: 'https://www.cyclestreets.net/journey/52327060/'
+					title: caption.substring (0, 20),
+					text: 'View this photo by ' + username + ' on CycleStreets',
+					url: 'https://www.mobiledesign.cyclestreets.net/photomap/' + locationId
 				};
-				console.log (shareData);
 				navigator.share (shareData);
 			});
 		},

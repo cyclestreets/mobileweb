21a74467232ae0c9098cf176ffea5536fb9184be
mvl22 <martin@lucas-smith.co.uk>
2020-12-27 12:48:55 +0000

    POIs: Clarify popupHtml

--- js/cyclestreets.js
+++ js/cyclestreets.js
@@ -256,10 +256,16 @@ var cyclestreetsui = (function ($) {
 			iconSize: [24, 24],
 			emptyPlaceholderText: '{%osmeditlink}',
 			popupHtml: 
-				  '<div class="data" data-coordinates="{geometry.coordinates}" data-name="{properties.name}" data-id="{properties.id}"></div><div class="place-photo">{%streetview}</div><a href="#" class="ui-button close-button" title="Close this popup"><img src="/images/icon-cross-red.svg" alt="Close icon" /></a><h2>{properties.name}</h2>'
+				  '<div class="data" data-coordinates="{geometry.coordinates}" data-name="{properties.name}" data-id="{properties.id}"></div>'
+				+ '<div class="place-photo">{%streetview}</div>'
+				+ '<a href="#" class="ui-button close-button" title="Close this popup"><img src="/images/icon-cross-red.svg" alt="Close icon" /></a>'
+				+ '<h2>{properties.name}</h2>'
 				+ '<a href="#" title="Get directions to this place"><img class="get-directions" src="/images/btn-get-directions-large.svg" /></a><p>{properties.osmTags.addr:street}</p>'
-				+ '<ul><li><img src="/images/icon-clock.svg" alt="Opening times" /><p>{properties.osmTags.opening_hours}</p></li><li>'
-				+ '<img src="/images/icon-telephone.svg" alt="Telephone contact" /><p class="phone">{properties.osmTags.phone}</p></li></ul><a href="#" class="share" title="Share this location"><img src="/images/icon-share.svg" alt="Share icon" /></a>',
+				+ '<ul>'
+				+ '<li><img src="/images/icon-clock.svg" alt="Opening times" /><p>{properties.osmTags.opening_hours}</p></li>'
+				+ '<li><img src="/images/icon-telephone.svg" alt="Telephone contact" /><p class="phone">{properties.osmTags.phone}</p></li>'
+				+ '</ul>'
+				+ '<a href="#" class="share" title="Share this location"><img src="/images/icon-share.svg" alt="Share icon" /></a>',
 			popupCallback: function (renderDetailsHtml, animation = false) {
 				cyclestreetsui.displayPoiPopup (renderDetailsHtml, animation);
 			}
